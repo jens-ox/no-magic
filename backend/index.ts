2f9c41f9a915bd2844fe326dfefa8d01ca2bf1e1
Jens Ochsenmeier <github@jens-ox.de>
2025-03-15 18:23:29 +0100

    cable together frontend and backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,11 @@
 import { Hono } from 'hono'
+import { join } from 'path'
+import { serveStatic } from 'hono/bun'
 const app = new Hono()
 
-app.get('/', (c) => c.text('Hono!'))
+console.log(import.meta.dir)
+
+app.get('*', serveStatic({ root: join(import.meta.dir, '../frontend/dist') }))
+app.get('*', serveStatic({ path: join(import.meta.dir, '../frontend/dist/index.html') }))
 
 export default app
